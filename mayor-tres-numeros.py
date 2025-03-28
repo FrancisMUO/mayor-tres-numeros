009e7a1b14da81817b573868819a249e20163e47
LAB_J205 <labj205@continental.edu.pe>
2025-03-28 11:22:00 -0500

    Clases para modularizar codigo

--- mayor-tres-numeros.py
+++ mayor-tres-numeros.py
@@ -1,24 +1,48 @@
 """
 Determina el mayor de tres números ingresados por el teclado
 """
-def intercambiar_valores(numero1, numero2):
-    temporal = numero1
-    numero1 = numero2
-    numero2 = temporal
-    return numero1, numero2
 
-numero1=int(input("Ingresa el primer numero : "))
-numero2=int(input("Ingresa el segundo numero: "))
-numero3=int(input("Ingresa el tercer numero : "))
+class OrdenarTresNumeros:
+    def __init__(self, numero1, numero2, numero3):
+        self.numero1 = numero1
+        self.numero2 = numero2
+        self.numero3 = numero3
 
-if numero1>numero2:
-   numero1, numero2 = intercambiar_valores(numero1, numero2)
+    def intercambiar_valores(self, numero1, numero2):
 
-if numero2>numero3:
-    numero2, numero3 = intercambiar_valores(numero2, numero3)
+        print(f"\nIntercambiando {numero1} y {numero2} ... \n")
+        temporal = numero1
+        numero1 = numero2
+        numero2 = temporal
+        return numero1, numero2
 
-if numero1>numero2:
-   numero1, numero2 = intercambiar_valores(numero1, numero2)
+    def ingreser_numeros(self):
+        print("\nIngrese los numeros ...")
 
-print(f"numero ordenados: {numero1}, {numero2}, {numero3}")
-print(f"El mayor es {numero3}")
+        self.numero1=int(input("Ingresa el primer numero : "))
+        self.numero2=int(input("Ingresa el segundo numero: "))
+        self.numero3=int(input("Ingresa el tercer numero : "))
+
+    def ordenar_numeros(self):
+
+        print("\nOrdenando ...")
+        if self.numero1 > self.numero2:
+            self. numero1, self.numero2 = self.intercambiar_valores(self.numero1, self.numero2)
+
+        if self.numero2 > self.numero3:
+            self.numero2, self.numero3 = self.intercambiar_valores(self.numero2, self.numero3)
+
+        if self.numero1 > self.numero2:
+           self.numero1, self.numero2 = self.intercambiar_valores(self.numero1, self.numero2)
+
+    def imprimir (self):
+        print("\nImprimiendo ... ")
+        print(f"numero ordenados: {self.numero1}, {self.numero2}, {self.numero3}")
+        print(f"El mayor es {self.numero3}")
+
+if __name__ == "__main__":
+    numero1, numero2, numero3 = 0, 0, 0
+    numeros = OrdenarTresNumeros(numero1, numero2, numero3)
+    numeros.ingreser_numeros()
+    numeros.ordenar_numeros()
+    numeros.imprimir()
